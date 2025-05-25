4b41c895e58691a1042abca49acfafee27de3967
3dpit <oksos0817@naver.com>
2025-05-25 22:46:05 +0900

    feat: 프론트 컨트롤러, 컨트롤러 매핑과 바인딩하는부분

--- servlet/src/main/java/dev/practice/servlet/HellobootController.java
+++ servlet/src/main/java/dev/practice/servlet/HellobootController.java
@@ -1,13 +1,8 @@
 package dev.practice.servlet;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HellobootController {
 
-    @GetMapping("/hello")
-    public String hello(String name){
-        return "Hello" + name;
+    public String hello(String name) {
+        return "Hello " + name;
     }
 }
